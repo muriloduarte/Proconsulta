da83db5d5a71e02397adf1cd142ad32cbeb5d345
Murilo Duarte <muriloduartegoncalves@hotmail.com>
2015-05-18 18:08:32 -0300

    applying loggin in users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,29 +8,33 @@ class UsersController < ApplicationController
 	# Returns all users
 	def allusers
 		@users = User.all
+		CUSTOM_LOGGER.info("Start to return users #{@user.to_yaml}")
 	end
 
 	# Return to index all users
 	def index
 		@user = User.all
+		CUSTOM_LOGGER.info("Start to return users #{@user.to_yaml}")
 	end
 
 	# Create new user
 	def new
 		@user = User.new
+		CUSTOM_LOGGER.info("Start to create user #{@user.to_yaml}")
 	end
 
 	# Create and associate one user to one id.
 	def create
 		@user = User.new(user_params)
-
 		# If user was save with succes, then sign in automaticly, else, render new.
 		if @user.save
 			sign_in @user
 		 	flash[:sucess] = "Seja bem-vindo!"
 			redirect_to :action => :show, :id => @user.id
+			CUSTOM_LOGGER.info("Created and associated user to params #{@user.to_yaml}")
 		else
 			render 'new'
+			CUSTOM_LOGGER.error("Not created and associated user to params #{@user.to_yaml}")
 		end
 	end
 
@@ -38,6 +42,7 @@ class UsersController < ApplicationController
 	def show
 		# Find all users with id passed by params.
 		@user = User.find(params[:id])
+		CUSTOM_LOGGER.info("Start to find users #{@user.to_yaml}")
 	end
 
 	# Edit an user.
@@ -47,8 +52,11 @@ class UsersController < ApplicationController
 		if !signed_in? || current_user != @user
 			redirect_to root_path
 			flash[:danger] = "Essa conta nao é sua!"
+			CUSTOM_LOGGER.error("There is no user session created #{@user.to_yaml}")
 		else
 			# nothing to do.
+			CUSTOM_LOGGER.warn("Nothing to do!")
+
 		end
 	end
 
@@ -62,8 +70,10 @@ class UsersController < ApplicationController
 			flash[:sucess] = "Usuário editado com exito!"
 			sign_in(@user)
 			redirect_to @user
+			CUSTOM_LOGGER.info("User edited successfully #{@user.to_yaml}")
 		else
 			render 'edit'
+			CUSTOM_LOGGER.error("User not updated and not found #{@user.to_yaml}")
 		end
 	end
 
@@ -76,10 +86,12 @@ class UsersController < ApplicationController
 		if current_user != @user
 			redirect_to root_path
 			flash[:error] = "Essa conta nao é sua!"
+			CUSTOM_LOGGER.error("Failure to destroy user #{@user.to_yaml}")
 		else
 			@user.destroy
 			redirect_to root_path
 			flash[:sucess] = "Usuário excluido com exito."
+			CUSTOM_LOGGER.info("User destroy successfully #{@user.to_yaml}")
 		end
 	end
 
@@ -90,5 +102,6 @@ class UsersController < ApplicationController
 															 :address_user, 
 															 :password_confirmation,
 															 :password)
+	CUSTOM_LOGGER.info("User params confirmation")
 	end
 end
